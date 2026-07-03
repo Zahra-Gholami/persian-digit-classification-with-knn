9fb934dc40ff923d7c8b3d8ca5d08f49766d94c6
Zahra-Gholami <zahra.gholami.mend@gmail.com>
2026-07-03 16:47:26 +0330

    debugging

--- dataset.py
+++ dataset.py
@@ -3,26 +3,24 @@ import cv2
 from scipy import io
 
 
-def load_hoda(training_Sample_Size=1000 , test_sample_size=200, size=5):
-    #load_dataset
-    dataset=io.loadmat('./dataset/Data_hoda_full.mat')
+def load_hoda(training_Sample_Size=1000, test_sample_size=200, size=5):
+    # بارگذاری دیتاست
+    dataset = io.loadmat('./dataset/Data_hoda_full.mat')
 
-    #test and training set
-    x_train_original = np.squeeze(dataset['Data'][training_Sample_Size])
-    y_train=np.squeeze(dataset['Label'][training_Sample_Size])
+    # استخراج تصاویر از ستون اول (هر عنصر یک تصویر است)
+    x_train_original = dataset['Data'][:training_Sample_Size, 0]  # ← کلید اصلی
+    x_test_original = dataset['Data'][training_Sample_Size:training_Sample_Size + test_sample_size, 0]
 
-    x_test_original = np.squeeze(dataset['Data'][training_Sample_Size:training_Sample_Size+test_sample_size])
-    y_test=np.squeeze(dataset['Label'][training_Sample_Size:training_Sample_Size+test_sample_size])
+    # برچسب‌ها
+    y_train = np.squeeze(dataset['labels'][:training_Sample_Size])
+    y_test = np.squeeze(dataset['labels'][training_Sample_Size:training_Sample_Size + test_sample_size])
 
-    #resize
-    x_train_5by5=[cv2.resize(img,desize=(size,size)) for img in x_train_original]
-    x_test_5by5=[cv2.resize(img, desize=(size,size)) for img in x_train_original]
+    # تغییر سایز به ۵×۵
+    x_train_5by5 = [cv2.resize(img, dsize=(size, size)) for img in x_train_original]
+    x_test_5by5 = [cv2.resize(img, dsize=(size, size)) for img in x_test_original]
 
-    #reshape
-    x_train=np.reshape(x_train_5by5,[-1,size**2])
-    x_test=np.reshape(x_test_5by5,[-1,size**2])
+    # تغییر شکل به بردار
+    x_train = np.reshape(x_train_5by5, [-1, size ** 2])
+    x_test = np.reshape(x_test_5by5, [-1, size ** 2])
 
-
-
-
-    return x_train, y_train ,x_test, y_test
+    return x_train, y_train, x_test, y_test
